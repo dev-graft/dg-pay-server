26f949dc80b0fae6597ba5efb84f8e0a1c215a5a
TAE HEON KWON <PCloud63514@gmail.com>
2023-05-29 20:59:12 +0900

    feat(point): 포인트 송금 기능 추가 피트(point): PointRemitUseCase 인터페이스 추가 피트(ServiceTransactionKey.kt): 거래 서명, 거래, 거래 이력, 포인트 등의 정보를 담는 ServiceTransactionKey 모델 추가 피트(ServiceTransactionKey.kt): 트랜잭션 아이디를 묶기 위해 ServiceTransactionKey 모델에 대한 주석 추가

--- dg-pay-domain/src/main/kotlin/devgraft/dgpay/domain/point/model/ServiceTransactionKey.kt
+++ dg-pay-domain/src/main/kotlin/devgraft/dgpay/domain/point/model/ServiceTransactionKey.kt
@@ -1,3 +1,20 @@
 package devgraft.dgpay.domain.point.model
 
 data class ServiceTransactionKey(val value: String)
+
+/**
+ * 거래 서명
+ * 거래
+ * 거래 이력
+ * 포인트
+ *
+ * 보내는 사람 privateKey
+ * 받는 사람 publicKey
+ * 보내는 금액
+ * 서비스 거래 설명
+ *
+ * 이러면 한쪽은 차감
+ * 한쪽은 적립인데
+ * 트랜잭션 아이디를 묶어야하나?
+ * 그게 맞겠지 뭐
+ */
--- dg-pay-domain/src/main/kotlin/devgraft/dgpay/domain/point/port/inbound/PointRemitUseCase.kt
+++ dg-pay-domain/src/main/kotlin/devgraft/dgpay/domain/point/port/inbound/PointRemitUseCase.kt
@@ -4,6 +4,9 @@ import devgraft.dgpay.domain.point.model.Amount
 import devgraft.dgpay.domain.point.model.ServiceDescription
 import devgraft.dgpay.domain.point.model.ServiceTransactionKey
 
+/**
+ * 포인트 송금
+ */
 interface PointRemitUseCase {
     fun remit(senderId: String, receiverId: String, amount: Amount, description: ServiceDescription): ServiceTransactionKey
 }
